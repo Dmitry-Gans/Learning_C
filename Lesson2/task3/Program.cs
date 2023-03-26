ebde83d5467a9a09ae0ab264b38b654508776627
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-03-26 18:43:10 +0300

    Подправил решение

--- Lesson2/task3/Program.cs
+++ Lesson2/task3/Program.cs
@@ -1,46 +1,43 @@
 ﻿/*Задача No14. Общее обсуждение
-Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Напишите программу, которая принимает на вход число и проверяет, 
+кратно ли оно одновременно 7 и 23.
 ● 14 -> нет
 ● 46 -> нет
 ● 161 -> да*/
+/*
+Console.WriteLine("Введите число которое будет кратным для 7 и 23");
+int num = int.Parse(Console.ReadLine());
 
-/*int num1 = int.Parse(Console.ReadLine());
-
-if (num1 % 7 == 0 & num1 % 23 == 0)
+if (num % 7 == 0 & num % 23 == 0)
 {
-    Console.WriteLine("The number is miltiple to botch 7 and 23");
+    Console.WriteLine($"Число {num} является кратным для 7 и 23");
 }
 else
 {
-    Console.WriteLine("The number isn't miltiple to botch 7 and 23");
+    Console.WriteLine($"Число {num} не вляется кратным для 7 и 23");
 }
 */
+/*Напишите программу, которая принимает на вход два числа и проверяет,
+является ли одно число квадратом другого.
+● 5, 25 -> да 
+● -4, 16 -> да
+● 25, 5 -> да
+● 8,9 -> нет*/
 
-/*Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
-● 5, 25 -> да ● -4, 16 -> да ● 25, 5 -> да ● 8,9 -> нет*/
-Console.WriteLine("Please input the first number");
+Console.WriteLine("Введите первое число");
 int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Please input the second number");
+Console.WriteLine("Введите второе число");
 int num2 = int.Parse(Console.ReadLine());
 
-/*if (num2 * num2 == num1 | num1 * num1 == num2)
+if (num1 * num1 == num2)
 {
-    Console.WriteLine("num1 is num2 squared or the opposite");
+    Console.WriteLine($"Число {num2} это {num1} в квадрате");
+}
+else if (num2 * num2 == num1)
+{
+    Console.WriteLine($"Число {num1} это {num2} в квадрате");
 }
 else
 {
-    Console.WriteLine("num1 of the numbers are squares of each other");
-}*/
-
-if (num2 * num2 == num1)
-{
-    Console.WriteLine("num1 is num2 squared");
-}
-else if (num1 * num1 == num2)
-{
-    Console.WriteLine("num2 is num1 squared");
-}
-else
-{
-    Console.WriteLine("num1 of the numbers are squares of each other");
+    Console.WriteLine($"Число {num1} не являются квадратом {num2}");
 }

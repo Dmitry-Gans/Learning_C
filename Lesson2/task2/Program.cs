a3ca437d575f7f15429854d9a9ee08f955684bc4
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-03-26 17:11:56 +0300

    Добавил описание команд

--- Lesson2/task2/Program.cs
+++ Lesson2/task2/Program.cs
@@ -19,12 +19,13 @@ Console.WriteLine(result);
 //Второй способ, просто через числа:
 
 Random rundom = new Random();
-int rundomValue = rundom.Next(100, 1000);
+int rundomValue = rundom.Next(100, 1000);//Получаем случайное число в диапазоне 100 - 1000
 
-int first = rundomValue / 100;
-int last = rundomValue % 10;
+int first = rundomValue / 100;//Получаем первое число
+int last = rundomValue % 10;//Получаем последнее число
 
-Console.WriteLine($"{rundomValue}->{first}{last}");
+Console.WriteLine($"{rundomValue}->{first}{last}");//Объединяем первое и последнее число 
+
 /*Задача No12. Работа в группах
 Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 ● 34, 5 -> не кратно, остаток 4

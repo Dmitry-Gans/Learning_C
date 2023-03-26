04ce72a9512b49b84b7354b42b8653bede7c949e
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-03-26 16:21:16 +0300

    Изменил название папки

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,36 +1,3 @@
-﻿/*int a = 5;
-
-int b = 6;
-
-int result = a * (a * b);
-
-int c = 7;
-
-int g = 8;
-
-int result1 = Calculate(a, b);
-Show(a,b)
-
-int result2 = Calculate(c, g);
-Show(c,g)
-
-Console.WriteLine(result1);
-Console.WriteLine(result2);
-
-int Calculate(int a, int b)
-{
-    returne a * (a * b) - 5;
-    
-    /*Обычный способ написания:
-    int result = a * (a * b) - 5;
-    return result;
-}
-/*void Show(int a, int b)
-{
-    Console.WriteLine($"{a} , {b}");
-}
-Console.WriteLine(result);*/
-
 /*Напишите программу, которая выводит случайное число из отрезка [10, 99]
 и покажите наибольшую цифру числа:
 */

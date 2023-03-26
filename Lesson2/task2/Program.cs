988a3991b5982bff9a7c150c47b05b763d8ea38f
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-03-26 17:56:45 +0300

    Подправил решени и добавил описание

--- Lesson2/task2/Program.cs
+++ Lesson2/task2/Program.cs
@@ -18,27 +18,31 @@ Console.WriteLine(result);
 
 //Второй способ, просто через числа:
 
-Random rundom = new Random();
+/*Random rundom = new Random();
 int rundomValue = rundom.Next(100, 1000);//Получаем случайное число в диапазоне 100 - 1000
 
 int first = rundomValue / 100;//Получаем первое число
 int last = rundomValue % 10;//Получаем последнее число
 
 Console.WriteLine($"{rundomValue}->{first}{last}");//Объединяем первое и последнее число 
+*/
 
-/*Задача No12. Работа в группах
-Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+/*Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому.
+Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 ● 34, 5 -> не кратно, остаток 4
 ● 16, 4 -> кратно*/
 
-/*int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
 int num2 = int.Parse(Console.ReadLine());
 
-if (num1 % num2 == 0)
+if (num1 % num2 == 0)//Приравниваем к 0, чтобы понять, что число кратное или нет
 {
-    Console.WriteLine($"{num1} кратное {num2}");
+    Console.WriteLine($"{num1}, {num2} кратно");
 }
 else
 {
-    Console.WriteLine($"{num1 % num2} не кратное");
-}*/
+    Console.WriteLine($"{num1 % num2} не кратно, остаток");
+}

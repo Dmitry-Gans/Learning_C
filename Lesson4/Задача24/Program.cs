f6b5b88a6dd9534a3838c21584f44ab1ddca3e70
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-04-06 20:41:14 +0300

    Подправил 24 задачу

--- Lesson4/Задача24/Program.cs
+++ Lesson4/Задача24/Program.cs
@@ -1,4 +1,7 @@
-﻿int a = int.Parse(Console.ReadLine());
+﻿//Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
 
 int sum = GetSumToNumber(a);
 Console.WriteLine(sum);
@@ -6,7 +9,7 @@ Console.WriteLine(sum);
 int GetSumToNumber(int number)
 {
     int sum = 0;
-    for (int i = 1; i < number; i++)
+    for (int i = 1; i <= number; i++)
     {
         //sum = sum + i;
         sum += i;

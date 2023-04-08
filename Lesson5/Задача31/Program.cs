33befdae127a8dd48a18aa04bc0e05f7558f0b4e
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-04-08 19:00:15 +0300

    Решил задачу 32

---
+++ Lesson5/Задача31/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Задача 31:
+Задайтте массив из 12 элементов, заполните случайными числами
+из промежутка [-9, 9].
+Найдите сумму отрицательных и положительных элементов масиива.
+Например:
+[3, 9, -8, 1, 0, -7, 2, -1, 8, -3, -1, 6] сумма положительных 
+чисел ровна 29, сумма отрицательных равна -20.
+*/
+int[] array = GenerationArray(12);
+PrintArray(array);
+
+int sum = CalculateSumPositive(array);
+int sum1 = CalculateSumNegative(array);
+Console.WriteLine($"Сумма положительных чисел = {sum}, Сумма отрицательных чисел = {sum1}");
+
+int[] GenerationArray(int lenght)//Созздаем метод для генирации масива и его длинны 
+{
+    int[] array = new int[lenght];
+
+    Random random = new Random();
+
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = random.Next(-9, 10);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");/*Вписываем в ковычках,
+    через какой символ будут чередоваться числа в массиве.
+    После ковычек ставим запятую и сам массив, который в последствии превращается
+    в строчку*/
+}
+
+int CalculateSumPositive(int[] array)
+{
+    int sumPositiv = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+        sumPositiv += array[i];//sumPositiv = sumPositiv + array[i]
+        }
+    }
+    return sumPositiv;
+}
+int CalculateSumNegative(int[] array)
+{
+    int sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0)
+        sumNegative += array[i];
+    }
+    return sumNegative;
+}
---
+++ Lesson5/Задача32/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 32: Напишите программу замена элементов массива:
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+int[] array = new int[5];
+
+FillArray(array);
+
+PrintArray(array);
+
+ChangeSignOfNumbers(array);
+
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 11);
+    }
+}
+void ChangeSignOfNumbers(int[] array)//Cоздаем метод для замены минуса/плюса
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = - array[i];
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}

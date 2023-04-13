5e2d6f459fdc9eaa6699293adcbe9e280a77d425
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-04-13 20:51:26 +0300

    Добавил рещение через массив, задача 44

--- Lesson6/Задача44/Program.cs
+++ Lesson6/Задача44/Program.cs
@@ -3,7 +3,7 @@
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
-
+/*
 Console.WriteLine("Введите число");
 double num = double.Parse(Console.ReadLine());
 
@@ -16,4 +16,34 @@ double FibonacciN(int num)
 for (int i = 0; i < num; i++)
 {
     Console.Write(FibonacciN(i));
+}
+*/
+//Решение через массив:
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int[] result = FibonacciN(num);
+PrintArray(result);
+int[] FibonacciN(int num)
+{
+    int[] array = new int[num];
+    //Вводим array 0 и 1, через условие, чтобы правильно заполненить массив
+    if (array.Length > 0)
+    {
+        array[0] = 0;
+    }
+    if (array.Length > 1)
+    {
+        array[1] = 1;
+    }
+    //В цикле начинаем с 2, так как 0 и 1 уже заполнили выше
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine(string.Join(", ", array));
 }

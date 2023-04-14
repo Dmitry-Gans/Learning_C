5a1be80ee2f39084002f7055effc9a70d8545a4f
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-04-15 00:30:22 +0300

    Запустил новый проект с TG Bot

---
+++ Bot_Telegram/Program.cs
@@ -0,0 +1,42 @@
+﻿// dotnet add package Telegram.Bot - подключаем нужную библиотеку дял рабоыт с ботом
+// Далее прописываем Юзинги 
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+
+//Создаем обьект класса "TelegramBotClient" работа с ним так же как и с Random
+//В ковычках указываем токен от тлеграм Бота
+TelegramBotClient client = new TelegramBotClient("6038050382:AAF-eRSW-QkRXr02IiMXZfN5WSxGHtNKXuE");
+
+//Узнаем пользователя
+User user = await client.GetMeAsync();
+//Показываем UserName бота
+Console.WriteLine(user.Username);
+
+//Чтобы программа работала бесконечно, запускаем цикл
+while (true)
+{
+    //Создаем массив обновлений: кто то добавился, написал и тд
+    Update[] update = await client.GetUpdatesAsync();
+    for (int i = 0; i < update.Length; i++)
+    {   //Показываем смс текст
+        Console.WriteLine(update[i].Message.Text);
+        //Показываем отправителя смс
+        Console.WriteLine(update[i].Message.From.FirstName);
+        //Можно добавлять условия
+        if(update[i].Message.Text == "Привет")
+        {   //Отправляем ответное смс и обращаемся к имени отправителя
+            await client.SendTextMessageAsync(update[i].Message.From.Id, $"Привет {update[i].Message.From.FirstName}");
+        }
+        //Показываем сообщение и его отправителя, отправляем ответное смс
+                                                   //смс   id отправителя        
+        //await client.SendTextMessageAsync(update[i].Message.From.Id, "Текст сообщения");
+    }
+    //Чтобы пропускать некоторые обновления
+    if (update.Length != 0)
+    {//Обращаемся к последнему обновлению Id, говорим, присылай все после него
+    //Чтобы консоль не показывала одно и тоже обноление повторно
+        update = await client.GetUpdatesAsync(update[update.Length-1].Id + 1);
+    } 
+}
--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,13 +1,13 @@
 ﻿int number = int.Parse(Console.ReadLine());
 
-int[] array = ConvertToArray(nnumber);
+int[] array = ReverseArray(number);
 
-int[] ReverseArray(int[] array)
+int[] ReverseArray(int number)
 {
     int length = new int[array.Length];
     int[] result = new int[];
 
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         result[i] = array;
         number = number / 10;
@@ -18,8 +18,8 @@ int GetLengthNumber(int number)
     int length = 0;
     while (number != 0)
     {
-        number = number / 10
-        length++
+        number = number / 10;
+        length++;
     }
     return length;
 }
